fe0f5ba6fd2f01def58f91a6560b9121d6ab67d0
homodeluna <1402692684@qq.com>
2025-12-07 00:18:02 +0800

    add stylua

--- init.lua
+++ init.lua
@@ -1,23 +1,21 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
+  vim.fn.system {
     "git",
     "clone",
     "--filter=blob:none",
     "git@github.com:folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
     lazypath,
-  })
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
-
-
 require("wang-lsp-config")
 require("nvim-treesitter").setup {
-  highlight = { enable = true }
+  highlight = { enable = true },
 }
 
 require("nvim-tree").setup()
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -21,13 +21,13 @@ return {
   "git@github.com:tanvirtin/monokai.nvim",
   {
     "git@github.com:kylechui/nvim-surround",
-    version = "*",             -- Use for stability; omit to use `main` branch for the latest features
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
-      require("nvim-surround").setup({
+      require("nvim-surround").setup {
         -- Configuration here, or leave empty to use defaults
-      })
-    end
+      }
+    end,
   },
   {
     "git@github.com:ellisonleao/gruvbox.nvim",
@@ -37,7 +37,7 @@ return {
     "git@github.com:aznhe21/actions-preview.nvim.git",
     config = function()
       vim.keymap.set({ "v", "n" }, "gf", require("actions-preview").code_actions)
-    end
+    end,
   },
   "git@github.com:lukas-reineke/lsp-format.nvim.git",
   {
@@ -51,20 +51,19 @@ return {
   },
   "git@github.com:pierreglaser/folding-nvim.git",
   {
-    'git@github.com:nvim-telescope/telescope.nvim',
-    dependencies = { 'git@github.com:nvim-lua/plenary.nvim' }
-
+    "git@github.com:nvim-telescope/telescope.nvim",
+    dependencies = { "git@github.com:nvim-lua/plenary.nvim" },
   },
   "git@github.com:rktjmp/lush.nvim",
   "git@github.com:homodeluna/pracale.nvim",
   {
     "kylechui/nvim-surround",
-    version = "*",             -- Use for stability; omit to use `main` branch for the latest features
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
-      require("nvim-surround").setup({
+      require("nvim-surround").setup {
         -- Configuration here, or leave empty to use defaults
-      })
-    end
-  }
+      }
+    end,
+  },
 }
